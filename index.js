b8d27e4cc9213b1750f5d0af966c8c764cff8899
James Maroko <jamiemaroko41@gmail.com>
2024-09-26 15:03:10 +0300

    lab4 complete

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+//is an equation whose multiplied results will equal 62
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+
+//Generates a random integer greater than 0
+let random = Math.floor(Math.random() * 10) + 1;
+console.log("Random number:", random);
+
+
+//Calculates a remainder that is equal to 4
+let num3 = 10;
+let num4 = 6;
+let mod = (num3 % num4);
+console.log(mod);
+
+//Returns 20 as the highest number in the set
+let numbers = [10, 20, 5, 15, 14];
+let max = Math.max(...numbers); // Use the spread operator
+console.log(max);
